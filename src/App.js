185ef764f474f1658d20501f696b99128d6f49eb
stefann06 <stefann06@yahoo.com>
2022-02-17 17:33:38 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='backgroundclr'>
+      <h1 className="text-center p-5">....under construction !!!</h1>
+      <h3 className="text-center p-5">My projects :</h3>
+      <ul className="text-center p-1">
+        <a href="https://the-90s-store.herokuapp.com/" target="_blank">E-commerce - Redux</a>
+        <br/>
+        <a href="https://marin-admin-panel.herokuapp.com/" target="_blank">Admin panel - React</a>
+        <br/>
+        <a href="https://marin-stefan.github.io/Gps-Tracker" target="_blank">Gps Ttracker - React</a>
+        <br/>
+        <a href="https://marin-stefan.github.io/Advanced-Calculator" target="_blank">Calculator - JS</a>
+        <br/>
+        <a href="https://marin-stefan.github.io/Vintage-Digital-Casio-Watch/" target="_blank">Watch - JS</a>
+        <br/>
+        <a href="https://marin-stefan.github.io/HtmlProj-BasicPizzaRestaurant/" target="_blank">Pizza restaurant - HTML/JS</a>
+        <br/>
+        <a href="https://marin-stefan.github.io/HTML.CSS-SimpleBlog/" target="_blank">Travel Blog - HTML</a>
+      </ul>
     </div>
+    // <div className="App">
+    //   <header className="App-header">
+    //     {/* <img src={logo} className="App-logo" alt="logo" /> */}
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
   );
 }
 
